bd2ce178bab44171bc81bedfb8543e97195b8c9b
Your Name <you@example.com>
2019-01-30 22:02:49 +0900

    update carrier wave

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -12,6 +12,6 @@ CarrierWave.configure do |config|
     region: 'ap-northeast-1'
   }
 
-  config.fog_directory  = 'freemarket41a-sample'
-  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/freemarket41a-sample'
+  config.fog_directory  = 'freemarket-m'
+  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/freemarket-m'
 end
